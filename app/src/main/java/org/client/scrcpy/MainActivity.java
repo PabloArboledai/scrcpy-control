ca47a64e7a9225428fd176d63abd0e208d6af2fc
Pablo Arboleda <pablo.megacuentas@gmail.com>
2026-07-05 18:42:34 +0000

    FIX: Add missing MotionEvent import to fix compilation

--- app/src/main/java/org/client/scrcpy/MainActivity.java
+++ app/src/main/java/org/client/scrcpy/MainActivity.java
@@ -27,6 +27,7 @@ import android.view.KeyEvent;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.view.WindowManager;
 import android.widget.AdapterView;
